8bc10bc349880bd1ba65190f41c8c23ed0c1cd26
somekindofpast <multandras@gmail.com>
2022-05-04 22:39:02 +0200

    Add UserStory to model layer

---
+++ src/test/java/com/codecool/supersprinter2000/SuperSprinter2000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.supersprinter2000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSprinter2000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
